2b562e8f9b77cf4144dea1458140e1df48135975
maxyee <eyaminkhan00@gmail.com>
2024-05-02 13:16:51 +0600

    added four API to fetch daily, hourly and time series data

--- FastAPI/forex-data-fastAPI/main.py
+++ FastAPI/forex-data-fastAPI/main.py
@@ -9,14 +9,6 @@ pp = PrettyPrinter()
 app = FastAPI()
 
 
-# @app.get("/")
-# async def slow_route():
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get("https://marketdata.tradermade.com/api/v1/live") as resp:
-#             data = await resp.json()
-#             # do something with data
-#             return data
-
 @app.get("/")
 async def root(request: Request):
     url = "https://marketdata.tradermade.com/api/v1/live"
@@ -30,32 +22,53 @@ async def root(request: Request):
     return {'data': result}
 
 
+@app.get("/daily")
+async def daily():
+    url = "https://marketdata.tradermade.com/api/v1/historical" 
+    date = "2021-03-15"
+    currency = "EURUSD"
+    api_key = "q_csXrJmlzDRc-8kkAkN"
+    querystring = {"currency":currency,"date":date, "api_key":api_key}
+    response = requests.get(url, params=querystring)
+    result = response.json()
+    print(result)
+    return {'data': result}
 
 
-@app.get('/get_data')
-def first_data():
-    api_url = "https://marketdata.tradermade.com/api/v1/live"
-    all_data = requests.get(api_url).json()
-    data1 = all_data[0]
-    return {'user': data1}
+@app.get("/min_hour")
+async def min_hour():
+    fx = ["EURUSD", "USDJPY"]
+    dates = ["2021-03-15-13:00"]
+    array = []
+    api_key = "q_csXrJmlzDRc-8kkAkN"
+    url = "https://marketdata.tradermade.com/api/v1/minute_historical"
+    for i in fx:
+        for date in dates: 
+            querystring = {"currency":i,"date_time":date, "api_key":api_key}
+            response = requests.get(url, params=querystring)
+            array.append(response.json())
+    result = array
+    print(result)
+    return {'data': result}
 
-@app.get('/get_data_2')
-async def second_data():
-    api_url = "https://marketdata.tradermade.com/api/v1/live"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(api_url)
-        all_data = response.json()
-        data2 = all_data[1]
-        return {'data': data2}
+@app.get("/time_series")
+async def time_series():
+    url = "https://marketdata.tradermade.com/api/v1/timeseries?"
+    currency="USDJPY"
+    api_key = "q_csXrJmlzDRc-8kkAkN"
+    start_date="2021-03-01"
+    end_date="2021-03-22"
+    format="split"
+    interval="daily"
+    querystring = {"currency":currency,"start_date":start_date, "api_key":api_key, 
+                   "end_date":end_date, "format":format, "interval": interval}
+    response = requests.get(url, params=querystring)
+    result = response.json()
+    print(result)
+    return {'data': result}
 
 
 
-@app.post("/")
-async def post():
-    return {"message" : "hello from post method route"}
 
 
-@app.put("/")
-async def put():
-    return {"message": "hello from put method route"}
 
